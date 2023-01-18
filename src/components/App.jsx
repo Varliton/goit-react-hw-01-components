5c4e8f7f2905eb48ba475081b26def67a84e01f6
Varliton <nbmusic2405@gmail.com>
2023-01-18 14:27:24 +0200

    add profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user'
+
 export const App = () => {
   return (
     <div
@@ -6,11 +9,17 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+      likes={user.stats.likes}></Profile>
     </div>
   );
 };
