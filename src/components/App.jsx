772591c19a7a8c021bfbc15bfd53fdebe90dd034
Varliton <nbmusic2405@gmail.com>
2023-01-19 16:24:35 +0200

    add friendlist and transaction history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-import user from '../data/user'
-import data from '../data/data'
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory'
+
+import user from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+
 
 export const App = () => {
   return (
@@ -24,6 +30,8 @@ export const App = () => {
       ></Profile>
       <Statistics title="Upload Stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
