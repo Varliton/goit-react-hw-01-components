4aad38aa6bbcbd8bea6764f7ebb1f238b83ee593
Varliton <nbmusic2405@gmail.com>
2023-01-19 14:45:40 +0200

    add proptypes and statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,29 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import user from '../data/user'
+import data from '../data/data'
 
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        color: '#010101'
+        color: '#010101',
       }}
     >
-    <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         followers={user.stats.followers}
         views={user.stats.views}
-      likes={user.stats.likes}></Profile>
+        likes={user.stats.likes}
+      ></Profile>
+      <Statistics title="Upload Stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
